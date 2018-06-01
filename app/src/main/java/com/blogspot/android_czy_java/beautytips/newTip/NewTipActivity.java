19e4e465c183b05a72ce6acf79d9dc3406fa5278
Jagoda <jagoda.sylwester@gmail.com>
2018-06-01 17:08:07 +0200

    Add fab and its animation on scrolling

---
+++ app/src/main/java/com/blogspot/android_czy_java/beautytips/newTip/NewTipActivity.java
@@ -0,0 +1,15 @@
+package com.blogspot.android_czy_java.beautytips.newTip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blogspot.android_czy_java.beautytips.R;
+
+public class NewTipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_tip);
+    }
+}
