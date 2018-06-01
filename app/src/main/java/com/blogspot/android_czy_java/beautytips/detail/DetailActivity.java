eb9247cf95c558b055963d5b822a379193d8d828
Jagoda <jagoda.sylwester@gmail.com>
2018-06-01 17:08:07 +0200

    Add fab and its animation on scrolling

--- app/src/main/java/com/blogspot/android_czy_java/beautytips/detail/DetailActivity.java
+++ app/src/main/java/com/blogspot/android_czy_java/beautytips/detail/DetailActivity.java
@@ -2,6 +2,7 @@ package com.blogspot.android_czy_java.beautytips.detail;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,7 +11,9 @@ import android.support.v7.widget.Toolbar;
 import android.transition.TransitionManager;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.blogspot.android_czy_java.beautytips.R;
@@ -28,6 +31,15 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.collapsing_toolbar_layout)
     CollapsingToolbarLayout mCollapsingToolbarLayout;
 
+    @BindView(R.id.fab)
+    FloatingActionButton mFab;
+
+    @BindView(R.id.detail_scroll_view)
+    ScrollView mScrollView;
+
+    @BindView(R.id.layout_ingredients)
+    View mLayoutIngredients;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +51,28 @@ public class DetailActivity extends AppCompatActivity {
 
         prepareToolbar();
 
+        prepareFab();
         }
 
     private void prepareToolbar() {
-        mCollapsingToolbarLayout.setTitle("Lorem ipsum");
+        mCollapsingToolbarLayout.setTitle("Lorem ipsum dolores ames");
+
+    }
+
+    private void prepareFab() {
+
+        mScrollView.getViewTreeObserver().addOnScrollChangedListener(
+                new ViewTreeObserver.OnScrollChangedListener() {
+                    @Override
+                    public void onScrollChanged() {
+                        if (mScrollView.getScrollY() < mLayoutIngredients.getHeight()){
+                            mFab.show();
+                        } else {
+                            mFab.hide();
+                        }
+                    }
+                }
+        );
     }
 
 }
