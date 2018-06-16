0d833589f2d627963ea51dc395fba1f2c05bc43e
Jagoda <jagoda.sylwester@gmail.com>
2018-06-16 11:53:07 +0200

    Last changes to dialog

--- app/src/main/java/com/blogspot/android_czy_java/beautytips/listView/view/NicknamePickerDialog.java
+++ app/src/main/java/com/blogspot/android_czy_java/beautytips/listView/view/NicknamePickerDialog.java
@@ -4,16 +4,17 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
 import com.blogspot.android_czy_java.beautytips.R;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
+
+import timber.log.Timber;
 
 public class NicknamePickerDialog extends DialogFragment {
 
@@ -37,10 +38,10 @@ public class NicknamePickerDialog extends DialogFragment {
                 new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                EditText nicknameEt = getDialog().findViewById(R.id.nickname_edit_text);
-                mDialogListener.onDialogSaveButtonClick(nicknameEt.getText().toString());
+
             }
         });
+
         return builder.create();
     }
 
@@ -67,4 +68,41 @@ public class NicknamePickerDialog extends DialogFragment {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        final AlertDialog dialog = (AlertDialog)getDialog();
+
+        /*
+        Prevent dialog from closing when clicking on activity in background - this is done because
+        we need user to choose nickname at the beginning
+        */
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+
+        /*
+        We also prevent dialog from closin when edit text is empty
+         */
+        dialog.getButton(Dialog.BUTTON_POSITIVE).setOnClickListener
+                (new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        EditText nicknameEt = getDialog().findViewById(R.id.nickname_edit_text);
+                        String nickname = nicknameEt.getText().toString().trim();
+                        Timber.d("Chosen nickname: " + nickname);
+                        if(TextUtils.isEmpty(nickname)) {
+                            Timber.d("Nickname is empty");
+                            getDialog().findViewById(R.id.error_text_view).setVisibility(View.VISIBLE);
+                        } else {
+                            mDialogListener.onDialogSaveButtonClick(nicknameEt.getText().toString());
+                            dialog.dismiss();
+
+                        }
+                    }
+                });
+
+
+    }
 }
