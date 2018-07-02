81d60e69923cf799ab1f3515b10fc0563ece85c0
Jagoda <jagoda.sylwester@gmail.com>
2018-07-02 16:38:28 +0200

    Improve nickname picker dialog

--- app/src/main/java/com/blogspot/android_czy_java/beautytips/listView/view/MainActivity.java
+++ app/src/main/java/com/blogspot/android_czy_java/beautytips/listView/view/MainActivity.java
@@ -5,7 +5,6 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
