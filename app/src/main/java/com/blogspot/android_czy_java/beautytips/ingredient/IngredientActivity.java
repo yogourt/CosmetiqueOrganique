388bf7ea4e90f2da77357da3ed377a43f59a7e56
Jagoda <jagoda.sylwester@gmail.com>
2018-07-21 14:16:41 +0200

    Add ingredient activity

--- app/src/main/java/com/blogspot/android_czy_java/beautytips/ingredient/IngredientActivity.java
+++ app/src/main/java/com/blogspot/android_czy_java/beautytips/ingredient/IngredientActivity.java
@@ -1,5 +1,6 @@
 package com.blogspot.android_czy_java.beautytips.ingredient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
@@ -9,18 +10,26 @@ import android.widget.TextView;
 
 import com.blogspot.android_czy_java.beautytips.R;
 import com.blogspot.android_czy_java.beautytips.detail.view.BaseItemActivity;
+import com.blogspot.android_czy_java.beautytips.listView.view.MainActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.haha.perflib.Main;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 public class IngredientActivity extends BaseItemActivity {
 
+    public static final String KEY_QUERY = "query";
+
     @BindView(R.id.properties_layout)
     LinearLayout mPropertiesLayout;
+
+    @BindView(R.id.searching_text_view)
+    TextView mSearchTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,8 @@ public class IngredientActivity extends BaseItemActivity {
     }
 
     private void prepareContent() {
+
+        mSearchTv.setText(getResources().getString(R.string.search_for_label, mTitle));
         FirebaseDatabase.getInstance().getReference("ingredients/" + mId).
                 addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
@@ -39,6 +50,7 @@ public class IngredientActivity extends BaseItemActivity {
                         for(DataSnapshot propertySnapshot: dataSnapshot.getChildren()) {
                             String description = String.valueOf(propertySnapshot.getValue());
                             String title = propertySnapshot.getKey();
+                            if(title.equals("1")) title = "Overview:";
 
                             View propertyView = LayoutInflater.from(IngredientActivity.this).
                                     inflate(R.layout.layout_ingredient_properties,
@@ -55,5 +67,12 @@ public class IngredientActivity extends BaseItemActivity {
                     public void onCancelled(@NonNull DatabaseError databaseError) {
                     }
                 });
+    }
+
+    public void searchFor(View view) {
+        Intent listViewIntent = new Intent(this, MainActivity.class);
+        listViewIntent.setAction(Intent.ACTION_SEARCH);
+        listViewIntent.putExtra(KEY_QUERY, mTitle);
+        startActivity(listViewIntent);
     }
 }
