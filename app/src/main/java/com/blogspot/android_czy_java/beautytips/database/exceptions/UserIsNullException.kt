1d57d31f3d15f285c5ce5697cdb8de4c31804654
Jagoda <jagoda.sylwester@gmail.com>
2019-06-28 00:43:49 +0200

    Remove db from master branch

--- app/src/main/java/com/blogspot/android_czy_java/beautytips/database/exceptions/UserIsNullException.kt
+++
@@ -1,7 +0,0 @@
-package com.blogspot.android_czy_java.beautytips.database.exceptions
-
-class UserIsNullException: Throwable() {
-
-    override val message: String?
-        get() = "Firebase user is null"
-}
